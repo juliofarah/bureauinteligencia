1f4e8dc72072beb3bec475cd032039ab737533df
Ramon Goncalves <raigons@gmail.com>
2012-01-19 14:27:39 -0200

    Populating country map. This map is used to insert values from a spreadsheet

--- core/Datacenter/CountryMap.php
+++ core/Datacenter/CountryMap.php
@@ -22,10 +22,17 @@ class CountryMap {
         $this->map->put("Brasil", 1);
         $this->map->put("Colombia",2);
         $this->map->put("Colômbia",2);
+        $this->map->put("Vietnã", 3);
+        $this->map->put("Guatemala",4);
+        $this->map->put("Peru", 5);
+        $this->map->put("Quênia", 6);
+        $this->map->put("Outros", 7);
     }
     
-    public function getCountryId($countryName){        
-        return $this->map->get($countryName);
+    public function getCountryId($countryName){
+        if($this->map->containsKey($countryName))
+            return $this->map->get($countryName);
+        return $this->getCountryId("Outros");
     }
 }
 
